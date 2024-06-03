16adaf538d9b4f9719a8d63611a8822ada9e5e89
Christian Betz <christian.betz@gmail.com>
2024-06-03 18:06:16 -0400

    POOL-2186 kernel changes for d98 and 6.6.x

--- drivers/net/wireless/intel/iwlwifi/iwl-drv.c
+++ drivers/net/wireless/intel/iwlwifi/iwl-drv.c
@@ -1866,6 +1866,7 @@ struct iwl_mod_params iwlwifi_mod_params = {
 	.bt_coex_active = true,
 	.power_level = IWL_POWER_INDEX_1,
 	.uapsd_disable = IWL_DISABLE_UAPSD_BSS | IWL_DISABLE_UAPSD_P2P_CLIENT,
+	.beacon_timeout = 16,
 	.enable_ini = ENABLE_INI,
 	/* the rest are 0 by default */
 };
@@ -1983,6 +1984,10 @@ module_param_named(enable_ini, iwlwifi_mod_params.enable_ini, uint, 0444);
 MODULE_PARM_DESC(enable_ini,
 		 "0:disable, 1-15:FW_DBG_PRESET Values, 16:enabled without preset value defined,"
 		 "Debug INI TLV FW debug infrastructure (default: 16)");
+
+module_param_named(beacon_timeout, iwlwifi_mod_params.beacon_timeout, uint, 0644);
+MODULE_PARM_DESC(beacon_timeout,
+		"Number of missed beacons before disconnecting (default: 16)");
 
 /*
  * set bt_coex_active to true, uCode will do kill/defer
